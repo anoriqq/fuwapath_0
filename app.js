e1320c2d967a62473e630f353fa6ddcbab1e990a
show0317sw <tunakan_8584@yahoo.co.jp>
2018-11-19 00:30:34 +0900

    helmetを適用(脆弱性対策)

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
