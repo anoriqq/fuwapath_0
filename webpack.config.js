c0f4ea921d6bb7e13c32371295f12a87859d1c42
show0317sw <tunakan_8584@yahoo.co.jp>
2018-11-19 00:00:22 +0900

    webpack適用

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './app/entry.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public/javascripts')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+};
