9afca5352c324b0fed1b283c4cc9d907eb47b73e
show0317sw <tunakan_8584@yahoo.co.jp>
2018-11-19 18:05:52 +0900

    テストを追加

---
+++ test/test.js
@@ -0,0 +1,39 @@
+'use strict';
+const request = require('supertest');
+const app = require('../app');
+const passportStub = require('passport-stub');
+
+describe('/login', ()=>{
+  before(()=>{
+    passportStub.install(app);
+    passportStub.login({user:{name: 'testuser'}});
+  });
+  after(()=>{
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
+
+  it('ログインのためのリンクが含まれる', (done)=>{
+    request(app)
+      .get('/login')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(/<a href="\/auth\/slack"/)
+      .expect(200, done);
+  });
+
+  it('ログイン時はユーザー名が表示される', (done)=>{
+    request(app)
+      .get('/login')
+      .expect(/testuser/)
+      .expect(200, done);
+  });
+});
+
+describe('/logout', ()=>{
+  it('/にリダイレクトされる', (done)=>{
+    request(app)
+      .get('/logout')
+      .expect('Location', '/')
+      .expect(302, done);
+  });
+});
