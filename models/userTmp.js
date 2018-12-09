a584a7aae662d90fa12ab82fb019c31785c2d3a2
show0317sw <tunakan_8584@yahoo.co.jp>
2018-12-10 08:05:37 +0900

    :sparkles: "userTmp"モデルの追加とモデルの変更

--- models/userTmp.js
+++ models/userTmp.js
@@ -2,26 +2,30 @@
 const loader = require('./_sequelize-loader');
 const Sequelize = loader.Sequelize;
 
-const User = loader.database.define('users', {
-  user_id: {
+const UserTmp = loader.database.define('user_tmp', {
+  email: {
     type: Sequelize.STRING,
-    primaryKey: true,
-    allowNull: false
+    primaryKey: true
   },
   username: {
     type: Sequelize.STRING,
     allowNull: false
   },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  token: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
   created_at:Sequelize.DATE,
-  updated_at:Sequelize.DATE,
-  deleted_at:Sequelize.DATE
+  updated_at:Sequelize.DATE
 }, {
   freezeTableName: true,
   timestamps: true,
   createdAt: 'created_at',
-  updatedAt: 'updated_at',
-  deletedAt: 'deleted_at',
-  paranoid: true
+  updatedAt: 'updated_at'
 });
 
-module.exports = User;
+module.exports = UserTmp;
