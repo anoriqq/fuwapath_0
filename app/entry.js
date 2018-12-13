a243ded42b9b3bd285ac7979cb374e0d97650fa8
show0317sw <tunakan_8584@yahoo.co.jp>
2018-12-13 13:15:20 +0900

    :sparkles: 管理者権限で通常イベントを追加する機能を実装

--- app/entry.js
+++ app/entry.js
@@ -2,3 +2,15 @@
 import $ from 'jquery';
 var global = Function('return this;')();
 global.jQuery = $;
+
+var btnAccountEdit = $('.btn-status-add');
+btnAccountEdit.on('click', ()=>{
+  const statusName = $('input[name="user_status_name"]').val();
+  $.ajax({
+    method:'PUT',
+    url: '/user/status',
+    data: {statusName: statusName}
+  }).done(()=>{
+    $('input[name="user_status_name"]').val('');
+  });
+});
--- routes/user.js
+++ routes/user.js
@@ -4,13 +4,52 @@ const router = express.Router();
 
 // モジュールの読み込み
 const authenticationEnsurer = require('./_authentication-ensurer');
+const crypto = require('crypto');
 
-// ~/user にGETアクセスが来たときの処理
+// モデルの読み込み
+const UserStatus = require('../models/userStatus');
+const Status = require('../models/statuses');
+
+// '~/user' にGETアクセスが来たときの処理
 router.get('/', authenticationEnsurer, function(req, res, next){
-  res.render('user', {
-    title: 'ユーザーページ | fuwapath',
-    user: req.user
+  Status.findAll().then(status=>{
+    res.render('user', {
+      title: 'ユーザーページ | fuwapath',
+      user: req.user,
+      status: status
+    });
   });
+});
+
+// '~/user/status' にPOSTアクセスが来たときの処理
+router.put('/userstatus', authenticationEnsurer, function(req, res, next){
+  const statusCode = crypto.randomBytes(8).toString('hex');
+  UserStatus.create({
+    status_code: statusCode,
+    user_id: req.user.user_id,
+    status_name: req.body.statusName
+  }).then(()=>{
+    res.end();
+  });
+});
+
+// '~/user/setting' にPUTアクセスが来たときの処理
+router.put('/status', authenticationEnsurer, function(req, res, next){
+  const statusName = req.body.statusName;
+  console.log(statusName);
+  if(req.user.user_id === 'admin' && statusName !== ''){
+    const statusCode = crypto.randomBytes(8).toString('hex');
+    Status.create({
+      status_code: statusCode,
+      status_name: statusName
+    }).then(()=>{
+      res.end();
+    }).catch((err)=>{
+      res.end(500);
+    });
+  }else{
+    res.end(404);
+  }
 });
 
 module.exports = router;
