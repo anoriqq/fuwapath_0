c76b5d818458ea8ba1b13374b317ad3a547f7ccd
show0317sw <tunakan_8584@yahoo.co.jp>
2018-12-21 21:58:53 +0900

    :sparkles: close #13 ログインのリダイレクト先をcookieで管理する機能を実装

--- routes/login.js
+++ routes/login.js
@@ -7,6 +7,10 @@ const passport = require('passport');
 
 // ~/login にGETアクセスが来たときの処理
 router.get('/', function(req, res, next){
+  const from = req.query.from;
+  if(from){
+    res.cookie('loginFrom', from, {expires: new Date(Date.now() + 60000)});
+  }
   res.render('login', {
     title: 'ログイン | fuwapath',
     user: req.user,
@@ -18,8 +22,14 @@ router.get('/', function(req, res, next){
 
 // ~/login にPOSTアクセスが来たときの処理
 router.post('/', (req, res, next)=>{
+  const loginFrom = req.cookies.loginFrom;
+  let redirectUrl = '/';
+  if(loginFrom && !loginFrom.includes('http://') && !loginFrom.includes('https://')){
+    res.clearCookie('loginFrom');
+    redirectUrl = loginFrom;
+  }
   passport.authenticate('local', {
-    successRedirect: '/',
+    successRedirect: redirectUrl,
     failureRedirect: '/login',
     failureFlash: true
   })(req, res, next);
