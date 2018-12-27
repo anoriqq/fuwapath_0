0c0f8231b3d375649f5fa2f72ca1729e376c0cc4
show0317sw <tunakan_8584@yahoo.co.jp>
2018-12-27 09:08:19 +0900

    :pencil2: sequelizeのlogingをデフォルトに変更

--- models/_sequelize-loader.js
+++ models/_sequelize-loader.js
@@ -1,6 +1,6 @@
 'use strict';
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/fuwapath', {logging: true, operatorsAliases: false, timezone: 'UTC'});
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/fuwapath', {operatorsAliases: false, timezone: 'UTC'});
 
 module.exports = {
   database: sequelize,
