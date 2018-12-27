26560655251950c6eb765ec10d1777f83fa84f53
show0317sw <tunakan_8584@yahoo.co.jp>
2018-12-27 09:22:34 +0900

    :pencil2: ファイル名の修正

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 // モデルの読み込み
-const Status = require('../models/aaastatuses');
+const Status = require('../models/statuses');
 const UserStatus = require('../models/userStatus');
 
 // '~/' にGETアクセスが来たときの処理
